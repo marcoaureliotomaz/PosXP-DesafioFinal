15a3b22ec299dfb01cc049d817abd064afe8b276
marco <marcoaureliotomaz@gmail.com>
2026-03-24 00:16:38 -0300

    feat: Commit Inicial

---
+++ src/main/java/br/com/posxp/clientesapi/ClientesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.posxp.clientesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesApiApplication.class, args);
+    }
+}
+
